80b6c1683e64f0392f927dc60f420d132761f543
Yunji-Yun <dbswlvkdlxld2022@naver.com>
2023-10-23 02:40:05 +0900

    [1022] week7 prac

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
